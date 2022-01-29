3b4756ceb81c4025ab05e89f4b7cdfde391ba800
JuanSe Restrepo <juanrestrepo91@gmail.com>
2022-01-28 21:28:01 -0500

    Despliegue Angular

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 require ('dotenv').config ();
 const express = require('express');
-const hbs = require('hbs');
+// const hbs = require('hbs');
 
 const app = express();
 const port = process.env.PORT;
 
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials');
+// app.set('view engine', 'hbs');
+// hbs.registerPartials(__dirname + '/views/partials');
 
 app.use (express.static ('public'));
 
-app.get ('/', (req, res) => {
-    res.render ('home', {
-        titulo: 'Curso de NodeJs',
-        nombre: 'Juan Sebastian'
-    });
-})
+// app.get ('/', (req, res) => {
+//     res.render ('home', {
+//         titulo: 'Curso de NodeJs',
+//         nombre: 'Juan Sebastian'
+//     });
+// })
 
-app.get ('/generic', (req, res) => {
-    res.render ('generic', {
-        titulo: 'Curso de NodeJs',
-        nombre: 'Juan Sebastian'
-    });
-})
+// app.get ('/generic', (req, res) => {
+//     res.render ('generic', {
+//         titulo: 'Curso de NodeJs',
+//         nombre: 'Juan Sebastian'
+//     });
+// })
 
-app.get ('/elements', (req, res) => {
-    res.render ('elements', {
-        titulo: 'Curso de NodeJs',
-        nombre: 'Juan Sebastian'
-    });
-})
+// app.get ('/elements', (req, res) => {
+//     res.render ('elements', {
+//         titulo: 'Curso de NodeJs',
+//         nombre: 'Juan Sebastian'
+//     });
+// })
 
 app.get ('*', (req, res) => {
-    res.sendFile (__dirname + '/public/404.html');
+    res.sendFile (__dirname + '/public/index.html');
 })
 
 app.listen(port, () => {
